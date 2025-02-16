1eca2bfcab32a4c096c16033be87a63632dd0452
Peter Rupnik <rupnikpeter@gmail.com>
2025-02-16 18:21:08 +0100

    Upped the image resolution and looked into Speaker_name attribute
    This assures there is no funny additional selection constraints
    on Speaker_name.

--- 1_inspect_previous_splits.py
+++ 1_inspect_previous_splits.py
@@ -31,17 +31,33 @@ print(
     .group_by("Speaker_gender country".split())
     .agg(pl.col("index").count())
 )
-dfp = df.to_pandas()
+# dfp = df.to_pandas()
+
+
+def to_int(l: list) -> list:
+    mapper = {name: i for i, name in enumerate(list(set(l)))}
+    return pl.Series([mapper[i] for i in l])
+
+
+df = df.with_columns(
+    # pl.col("Speaker_name").map_batches(to_int),
+    # pl.col("Speaker_gender").map_batches(to_int),
+    pl.col("Speaker_birth").map_batches(to_int),
+    pl.col("Speaker_party").map_batches(to_int),
+)
+
 print("Plotting")
-for feature in "audio_length index Speaker_gender Speaker_role Speaker_birth Speaker_MP Speaker_party Party_status".split():
+for feature in "Speaker_name audio_length index Speaker_gender Speaker_role Speaker_birth Speaker_MP Speaker_party Party_status".split():
     sns.displot(
-        dfp,
+        df.select(["in_sample", "country", feature]).to_pandas(),
         x=feature,
         hue="in_sample",
         col="country",
         stat="density",
         common_norm=False,
-        multiple="dodge",
+        multiple="dodge" if feature != "Speaker_name" else "stack",
     )
-    plt.gcf().savefig(f"1_images/{feature}.png")
+    if feature == "Speaker_name":
+        plt.gca().set_xticklabels([])
+    plt.gcf().savefig(f"1_images/{feature}.png", dpi=400)
 2 + 2
